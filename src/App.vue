677c09104e4dc059b39ed0c7007dca96fe9fc87c
student54 <student54@pcoip.ki.agh.edu.pl>
2022-05-28 14:14:48 +0200

    keyup enter is working

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
     <h1> Twój email to: {{ email }}</h1>
-
-    <input type="text" v-model="email">
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+    <input type="email" v-on:keyup.enter="alertMyEmail" v-model="email">
+    <button @click="alertMyEmail()">Wyświetl email</button>
   </div>
+
+
+
 </template>
 
 <script>
@@ -13,6 +19,11 @@ export default {
     return {
       email: '',
     };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
   }
 }
 
